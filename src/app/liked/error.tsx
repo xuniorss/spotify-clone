6ed7beb7ece0859c927486b1324d338c73b6449a
xuniorss <fortunatojunior2020@gmail.com>
2023-06-15 12:48:27 -0300

    Stripe integration

---
+++ src/app/liked/error.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Box } from '../../components/Box'
+
+export default function Error() {
+   return (
+      <Box className="flex h-full items-center justify-center">
+         <div className="text-neutral-400">Algo parece errado.</div>
+      </Box>
+   )
+}
